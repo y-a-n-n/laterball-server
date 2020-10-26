05f8df256a8b302f33de212b4fddb9fdcf727fc9
Yann <yann.frizenschaf@gmail.com>
2020-10-27 10:29:10 +1030

    Add deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 cp resources/application.conf resources/app.conf
 envsubst < resources/application.conf > resources/app2.conf
-cp resources/app2.conf resources/applicationpp.conf
-
-# ./gradlew appengineDeploy
-# cp resources/app.conf resources/application.conf
+mv resources/app2.conf resources/application.conf
+./gradlew appengineDeploy
+mv resources/app.conf resources/application.conf
